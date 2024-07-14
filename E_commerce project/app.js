09d5ca7769b67ce6eaec81a873962bd34a135e10
MissoumYoucef <Missoumleetcoding@gmail.com>
2024-07-14 10:57:08 +0100

    .

--- E_commerce project/app.js
+++ E_commerce project/app.js
@@ -1,48 +1,35 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-// const session = require('express-session'); 
-const cookieParser = require('cookie-parser');
-const productRoutes = require('./routes/product_routes.js');
-const connectDB = require('./db/db.js'); 
-const cors = require('cors');
-require('dotenv').config(); 
+const connectDB = require('./config/database'); 
+const passport = require('./config/passport');
+const productRoutes = require('./routes/productRoutes'); 
+const categoryRoutes = require('./routes/categoryRoutes');
+const userRoutes = require('./routes/authRoutes');
+const ordeRoutes = require('./routes/orderRoutes');
+require('dotenv').config();
+// ... other imports
 
 const app = express();
 
-// Connect to the database before starting the server
-const startServer = async () => {
-    try {
-        await connectDB(); 
-        const PORT = process.env.PORT || 3000;
-        app.listen(PORT, () => {
-            console.log(`Server running on port ${PORT}`);
-        });
-    } catch (err) {
-        console.error('Could not connect to the database:', err);
-        process.exit(1); 
-    }
-}
-
-startServer(); 
-
 // Middleware
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cookieParser());
-app.use(cors()); // Enable CORS 
-// app.use(session({
-//     secret: process.env.SESSION_SECRET, 
-//     resave: false,
-//     saveUninitialized: true 
-// })); 
+// MongoDB Connection
+const dbURI = process.env.MONGODB_URI; 
+connectDB(dbURI); 
 
+// JWT
+app.use(express.urlencoded({ extended: true })); 
+app.use(express.json()); 
+app.use(passport.initialize());
+
+
+// Routes
 app.use('/api/products', productRoutes);
+app.use('/api/categories', categoryRoutes);
+app.use('/api/user',userRoutes); 
+app.use('/api/orders', require('./middleware/auth').protect, ordeRoutes); 
 
-// Serve static files from the 'public' directory
-app.use(express.static('public'));
-// Error handling middleware
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ message: 'Something broke!' });
-});
 
+// Start the server 
+const port = process.env.PORT; // Use environment port or 5000 as default
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+}); 
--- E_commerce project/models/product.js
+++ E_commerce project/models/product.js
@@ -1,11 +1,46 @@
-// models/product.js
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const productSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    price: { type: Number, required: true },
-    description: String,
-    imageUrl: String, 
-});
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true,
+        min:0
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    category: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Category', 
+        required: true 
+    },
+    sizes:[
+        {
+            type: String,
+            enum: ['S', 'M', 'L', 'XL', 'XXL']
+        }
+    ],
+    colors: [{
+        type: String,
+    }],
+    stockQuantity: {
+        type: Number,
+        required: true,
+        min: 0,
+        default: 0, 
+    },
+    imagePath: {
+        type: String,
+        required: true,
+      },
+},{
+    timestamps: true, // Adds createdAt and updatedAt fields
+  });
+  
 
 module.exports = mongoose.model('Product', productSchema);
